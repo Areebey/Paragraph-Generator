1d2824ef4ef0fd3c2f6c0f43c18f54b9ef33da43
Areebey <areebrafiq125@gmail.com>
2023-02-21 02:44:48 +0500

    new commit

--- script.js
+++ script.js
@@ -3,17 +3,27 @@
 const input=document.getElementById("noOfWords");
 const container=document.querySelector(".container")
 
-let noOfWords;
 
+const generateWord = (n) =>{
+    let text="";
+    const letters="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(let i=0;i<n;++i){
+        const random=(Math.random()*25).toFixed(0);
+        text += letters[random];
+    }
+    return text;
+}
+console.log(generateWord(15))
+
+
+let noOfWords;
 const generatePara =() =>{
     noOfWords=Number(input.value);
 
-
-
     let para=document.createElement("p");
     para.innerText="Sample Text"
+
     para.setAttribute("class","mypara");
     container.append(para);
-
 
 }
